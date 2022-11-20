43651a9d26814f0f21fe999b41934ac215736c4f
caiovictormc <caiovictormc@gmail.com>
2022-11-20 17:20:10 -0300

    main: Color update form

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
 
 #include <imgui-SFML.h>
 #include <imgui.h>
+#include <SFML/Graphics.hpp>
+
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
+
+    sf::RectangleShape shape;
+    shape.setSize(sf::Vector2f(100, 100));
+    shape.setFillColor(sf::Color::Yellow);
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -27,12 +33,37 @@ int main() {
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::Text("Hello, world %d", 123);
+        ImGui::Begin("Demo window");
+        ImGui::Button("Hello!");
 
-        // ImGui::ShowDemoWindow();
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Openx..", "Ctrl+O")) { /* Do stuff */ }
+                if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
+                if (ImGui::MenuItem("Close", "Ctrl+W"))  { }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        float  my_color[3];
+        ImGui::ColorEdit3("Color", my_color);
+
+        shape.setFillColor(
+            sf::Color(
+                (sf::Uint8)my_color[0] * 255,
+                (sf::Uint8)my_color[1] * 255,
+                (sf::Uint8)my_color[2] * 255
+            )
+        );
+
+        ImGui::End();
 
         window.clear();
         ImGui::SFML::Render(window);
+        window.draw(shape);
         window.display();
     }
 
